b9a9367710cd91055cfd6329951677a014186c79
woopwoopthatsthesoundofthepuneet <puneet8205@gmail.com>
2020-04-30 00:15:46 -0400

    navbar for login now its own file

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <head>
-	<title>EHGP Forgot Password</title>
+	<title>EHGP Login</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -28,19 +28,3 @@
     </div>
   </nav>
 </header>
-<body class="d-flex flex-column h-100">
-    <!-- Begin page content -->
-<div class="container h-100">
-  <div class="row h-100 justify-content-center align-items-center">
-  <div class="container">
-    <h1 class="mt-5">Forgot your password?</h1>
-    <p class="lead">Please email Mr. Jacobs at mjacobs@holyghostprep.org for assistance.</p>
-    <p><a href="testind.php">Click here to return to the login page.</a></p>
-  </div>
-<p>
-    <a href = "https://youtu.be/dQw4w9WgXcQ?t=42" target="_blank">
-        <img src="forgotpassgif.gif" class="rounded float-left" alt="...">
-    </a>
-<p>
-</div>
-</main>
--- testindfailed.php
+++ testindfailed.php
@@ -241,37 +241,10 @@ include 'includeInc_2.php';
         }
     }
     </style>
-    
-<head>
-	<title>EHGP Login</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-</head>
-<!--Coded with love by Mutiullah Samim-->
-<header>
-  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
-  <div class="topnav-centered">
-      <ul class="navbar-nav mr-auto">
-        <a class="navbar-brand" href="testind.php">
-            <li class="nav-item">
-            <img src="./firebird.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            EHGP
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.holyghostprep.org/calendar?cal_date=2020-04-01">Calendar</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://ehgp.holyghostprep.org/announcements.php">Announcements</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="./BellSchedules.pdf">Bell Schedules</a>
-          </li>
-      </ul>
-    </div>
-  </nav>
-</header>
+<?php 
+require 'navbar.php';
+?>
+<!-- insert navbar above-->
 <body>
     <div class="fullscreen-bg">
     <video loop muted autoplay autplay poster="tempbackground.PNG" class="fullscreen-bg__video">
